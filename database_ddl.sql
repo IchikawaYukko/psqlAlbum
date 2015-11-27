a818ed73c1fbff92be23faa01d18923cf9c5c655
IchikawaYukko <ichikawayuriko@yahoo.co.jp>
2015-11-27 19:09:20 +0900

    [add] idetail.php にて YouTube埋め込み再生 Issue #2

--- database_ddl.sql
+++ database_ddl.sql
@@ -104,114 +104,6 @@ CREATE TABLE jpeg_orientation (
 ALTER TABLE public.jpeg_orientation OWNER TO yuriko;
 
 --
--- Name: nanjing2012; Type: TABLE; Schema: public; Owner: yuriko; Tablespace: 
---
-
-CREATE TABLE nanjing2012 (
-    id bigint NOT NULL,
-    date_begin date,
-    date_end date,
-    title text,
-    description text,
-    path_photo character varying(64)
-);
-
-
-ALTER TABLE public.nanjing2012 OWNER TO yuriko;
-
---
--- Name: nanjing2012_id_seq; Type: SEQUENCE; Schema: public; Owner: yuriko
---
-
-CREATE SEQUENCE nanjing2012_id_seq
-    START WITH 1
-    INCREMENT BY 1
-    NO MAXVALUE
-    NO MINVALUE
-    CACHE 1;
-
-
-ALTER TABLE public.nanjing2012_id_seq OWNER TO yuriko;
-
---
--- Name: nanjing2012_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: yuriko
---
-
-ALTER SEQUENCE nanjing2012_id_seq OWNED BY nanjing2012.id;
-
-
---
--- Name: nanjing2012gpx; Type: TABLE; Schema: public; Owner: yuriko; Tablespace: 
---
-
-CREATE TABLE nanjing2012gpx (
-    id bigint NOT NULL,
-    date date,
-    filename character varying(64)
-);
-
-
-ALTER TABLE public.nanjing2012gpx OWNER TO yuriko;
-
---
--- Name: nanjing2012gpx_id_seq; Type: SEQUENCE; Schema: public; Owner: yuriko
---
-
-CREATE SEQUENCE nanjing2012gpx_id_seq
-    START WITH 1
-    INCREMENT BY 1
-    NO MAXVALUE
-    NO MINVALUE
-    CACHE 1;
-
-
-ALTER TABLE public.nanjing2012gpx_id_seq OWNER TO yuriko;
-
---
--- Name: nanjing2012gpx_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: yuriko
---
-
-ALTER SEQUENCE nanjing2012gpx_id_seq OWNED BY nanjing2012gpx.id;
-
-
---
--- Name: nanjing2012photo; Type: TABLE; Schema: public; Owner: yuriko; Tablespace: 
---
-
-CREATE TABLE nanjing2012photo (
-    id bigint NOT NULL,
-    filename character varying(64) NOT NULL,
-    datetaken date NOT NULL,
-    title text,
-    description text,
-    flag character(3)
-);
-
-
-ALTER TABLE public.nanjing2012photo OWNER TO yuriko;
-
---
--- Name: nanjing2012photo_id_seq; Type: SEQUENCE; Schema: public; Owner: yuriko
---
-
-CREATE SEQUENCE nanjing2012photo_id_seq
-    START WITH 1
-    INCREMENT BY 1
-    NO MAXVALUE
-    NO MINVALUE
-    CACHE 1;
-
-
-ALTER TABLE public.nanjing2012photo_id_seq OWNER TO yuriko;
-
---
--- Name: nanjing2012photo_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: yuriko
---
-
-ALTER SEQUENCE nanjing2012photo_id_seq OWNED BY nanjing2012photo.id;
-
-
---
 -- Name: photo; Type: TABLE; Schema: public; Owner: yuriko; Tablespace: 
 --
 
@@ -266,13 +158,14 @@ ALTER TABLE public.photo_view OWNER TO yuriko;
 --
 
 CREATE TABLE video (
-    id bigint NOT NULL,
+    id integer NOT NULL,
     filename character varying(64) NOT NULL,
     datetaken date NOT NULL,
     title text,
     description text,
     length integer NOT NULL,
-    flag character(3)
+    flag character(3),
+    youtube_id text
 );
 
 
@@ -317,27 +210,6 @@ ALTER TABLE ONLY gpx ALTER COLUMN id SET DEFAULT nextval('gpx_id_seq'::regclass)
 -- Name: id; Type: DEFAULT; Schema: public; Owner: yuriko
 --
 
-ALTER TABLE ONLY nanjing2012 ALTER COLUMN id SET DEFAULT nextval('nanjing2012_id_seq'::regclass);
-
-
---
--- Name: id; Type: DEFAULT; Schema: public; Owner: yuriko
---
-
-ALTER TABLE ONLY nanjing2012gpx ALTER COLUMN id SET DEFAULT nextval('nanjing2012gpx_id_seq'::regclass);
-
-
---
--- Name: id; Type: DEFAULT; Schema: public; Owner: yuriko
---
-
-ALTER TABLE ONLY nanjing2012photo ALTER COLUMN id SET DEFAULT nextval('nanjing2012photo_id_seq'::regclass);
-
-
---
--- Name: id; Type: DEFAULT; Schema: public; Owner: yuriko
---
-
 ALTER TABLE ONLY photo ALTER COLUMN id SET DEFAULT nextval('photo_id_seq'::regclass);
 
 
@@ -349,67 +221,35 @@ ALTER TABLE ONLY video ALTER COLUMN id SET DEFAULT nextval('video_id_seq'::regcl
 
 
 --
--- Name: jpeg_orientation_pkey; Type: CONSTRAINT; Schema: public; Owner: yuriko; Tablespace: 
---
-
-ALTER TABLE ONLY jpeg_orientation
-    ADD CONSTRAINT jpeg_orientation_pkey PRIMARY KEY (orientation);
-
-
---
--- Name: nanjing2012_pkey; Type: CONSTRAINT; Schema: public; Owner: yuriko; Tablespace: 
---
-
-ALTER TABLE ONLY nanjing2012
-    ADD CONSTRAINT nanjing2012_pkey PRIMARY KEY (id);
-
-
---
--- Name: nanjing2012gpx_pkey; Type: CONSTRAINT; Schema: public; Owner: yuriko; Tablespace: 
---
-
-ALTER TABLE ONLY nanjing2012gpx
-    ADD CONSTRAINT nanjing2012gpx_pkey PRIMARY KEY (id);
-
-
---
--- Name: nanjing2012photo_pkey; Type: CONSTRAINT; Schema: public; Owner: yuriko; Tablespace: 
---
-
-ALTER TABLE ONLY nanjing2012photo
-    ADD CONSTRAINT nanjing2012photo_pkey PRIMARY KEY (id);
-
-
---
--- Name: nanjing2012video_pkey1; Type: CONSTRAINT; Schema: public; Owner: yuriko; Tablespace: 
---
-
-ALTER TABLE ONLY video
-    ADD CONSTRAINT nanjing2012video_pkey1 PRIMARY KEY (id);
-
-
---
--- Name: nanjing_gpx_pkey; Type: CONSTRAINT; Schema: public; Owner: yuriko; Tablespace: 
---
-
-ALTER TABLE ONLY gpx
-    ADD CONSTRAINT nanjing_gpx_pkey PRIMARY KEY (id);
-
-
---
--- Name: nanjing_photo_pkey; Type: CONSTRAINT; Schema: public; Owner: yuriko; Tablespace: 
---
-
-ALTER TABLE ONLY photo
-    ADD CONSTRAINT nanjing_photo_pkey PRIMARY KEY (id);
-
-
---
--- Name: nanjing_pkey; Type: CONSTRAINT; Schema: public; Owner: yuriko; Tablespace: 
+-- Name: album_pkey; Type: CONSTRAINT; Schema: public; Owner: yuriko; Tablespace: 
 --
 
 ALTER TABLE ONLY album
-    ADD CONSTRAINT nanjing_pkey PRIMARY KEY (id);
+    ADD CONSTRAINT album_pkey PRIMARY KEY (id);
+
+
+--
+-- Name: gpx_pkey; Type: CONSTRAINT; Schema: public; Owner: yuriko; Tablespace: 
+--
+
+ALTER TABLE ONLY gpx
+    ADD CONSTRAINT gpx_pkey PRIMARY KEY (id);
+
+
+--
+-- Name: photo_pkey; Type: CONSTRAINT; Schema: public; Owner: yuriko; Tablespace: 
+--
+
+ALTER TABLE ONLY photo
+    ADD CONSTRAINT photo_pkey PRIMARY KEY (id);
+
+
+--
+-- Name: video_pkey; Type: CONSTRAINT; Schema: public; Owner: yuriko; Tablespace: 
+--
+
+ALTER TABLE ONLY video
+    ADD CONSTRAINT video_pkey PRIMARY KEY (id);
 
 
 --
--- pvideo.php
+++ pvideo.php
@@ -3,31 +3,37 @@ require_once(dirname(__FILE__).'/palbum_object.php');
 require_once('settings.php');
 
 class Video extends AlbumObject {
-  private $db_id, $filename, $date, $title, $description, $length;
+  private $db_id, $filename, $date, $title, $description, $length, $youtube_id;
 
   public function __construct($id) {
     global $db, $psqlAlbum;
     
     if("array" == gettype($id)) {
-      $this->db_id = $id['id'];
-      $this->filename = $psqlAlbum['VideoDir'];
-      $this->filename = $this->filename.$id['filename'];
-      $this->date = $id['datetaken'];
-      $this->title = $id['title'];
+      //引数idが配列の場合
+      $this->db_id	= $id['id'];
+      $this->filename	= $psqlAlbum['VideoDir'];
+      $this->filename	= $this->filename.$id['filename'];
+      $this->date	= $id['datetaken'];
+      $this->title	= $id['title'];
       $this->description = $id['description'];
-      $this->length = $id['length'];
+      $this->length	= $id['length'];
+      $this->youtube_id	= $id['youtube_id'];
     } else {
+      //引数idが数値(DBのPRIMARY KEY)の場合
       //Connect DB and fetch rows.
       $this->db_id = $id;
 
-      $db->query("SELECT video.id AS id,filename,datetaken,video.title AS title,video.description AS description,length,path_photo FROM album,video WHERE datetaken <= date_end AND datetaken >= date_begin AND video.id = $1 ORDER BY video.id;", array($id));
+      //$db->query("SELECT video.id AS id,filename,datetaken,video.title AS title,video.description AS description,length,path_photo FROM album,video WHERE datetaken <= date_end AND datetaken >= date_begin AND video.id = $1 ORDER BY video.id;", array($id));
+      $db->query("SELECT * FROM video WHERE id = $1;", array($id));
       $result = $db->result_rows();
-      $this->filename = $psqlAlbum['VideoDir'];
-      $this->filename = $this->filename.$result['filename'];
-      $this->date = $result['datetaken'];
-      $this->title = $result['title'];
+
+      $this->filename	= $psqlAlbum['VideoDir'];
+      $this->filename	= $this->filename.$result['filename'];
+      $this->date	= $result['datetaken'];
+      $this->title	= $result['title'];
       $this->description = $result['description'];
-      $this->length = $result['length'];
+      $this->length	= $result['length'];
+      $this->youtube_id = $result['youtube_id'];
     }
   }
 
@@ -54,11 +60,19 @@ HEREDOC;
 
     $date = DBConn::date_toJapanese($this->date);
     $dir = $psqlAlbum['VideoDir'];
-  
+
+    if(is_null($this->youtube_id)) {
+      //youtube_idがnullの場合はHTML5の<video>でサーバのmp4ファイルを再生。(重い…)
+      $videotag = "<VIDEO src=\"$this->filename\" alt=\"$this->title\">";
+    } else {
+      //youtube_idがnullでない(YouTubeに動画がある)場合は埋め込んで再生(推奨)
+      $videotag = "<iframe width=\"960\" height=\"720\" src=\"https://www.youtube.com/embed/$this->youtube_id\" frameborder=\"0\" allowfullscreen></iframe>";
+    }
+
     return 
 <<<HEREDOC
 <DIV>
-  <VIDEO src="$this->filename" alt="$this->title">
+  $videotag
   <DIV>$this->title</DIV>
   <DIV>$this->description</DIV>
   <DIV>$date</DIV>
