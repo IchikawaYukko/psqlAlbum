63a30c49773fe08d4d83e1c0394acd239080b590
IchikawaYukko <ichikawayuriko@yahoo.co.jp>
2016-03-02 20:01:15 +0900

    [fix]ビデオ詳細表示時のエラー発生を解消
    詳細表示時の Fatal error:  Call to undefined method Video::getFileURL() を解消

--- pvideo.php
+++ pvideo.php
@@ -115,5 +115,10 @@ HEREDOC;
   public function getFilename() {
     return $this->filename;
   }
+  public function getFileURL() {
+    global $psqlAlbum;
+
+    return $psqlAlbum['AlbumRoot'] . $psqlAlbum['PhotoDir'] . $this->filename;
+  }
 }
 ?>
