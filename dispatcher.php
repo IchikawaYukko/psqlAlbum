f40e4b4e015d417d0857b4f8dac911a16f0272aa
IchikawaYukko <ichikawayuriko@yahoo.co.jp>
2018-10-14 18:40:28 +0000

    Replace Japanese comment by English

--- dispatcher.php
+++ dispatcher.php
@@ -1,15 +1,16 @@
 <?php
   //URLの引数を見て、適切なphpコードを呼び出す。
+  //See URL Parameters and call appropriate code.
 
   if(isset($_GET['pid']) or isset($_GET['vid']) or isset($_GET['sid'])) {
-    //オブジェクト詳細表示
+    // Show object detail
     require_once('idetail.php');
   } elseif(isset($_GET['aid'])) {
-    //アルバム
+    // Show Album
     $_GET['id'] = $_GET['aid'];
     require_once('ialbum.php');
   } elseif(isset($_GET['query'])) {
-    //検索
+    // Search
     require_once('isearch.php');
   } elseif(isset($_GET['type'])) {
     require_once('ialbum.php');
@@ -23,6 +24,6 @@
     // Show Logout page
     require_once('psqlAlbum-admin/ilogout.php');
   } elseif(empty($_GET)) {
-    //アルバム一覧(トップページ)
+    // Show album list (top page)
     require_once('iindex.php');
   }
